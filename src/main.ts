5ef09d59d62d3da713328b671a8d35cda1947a9e
spanwite <spanwite@ya.ru>
2025-11-08 23:01:57 +0300

    feat(app): create entry point

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { Database } from 'bun:sqlite';
+import TelegramBot from 'node-telegram-bot-api';
+import { config } from './config.ts';
+import { logger } from './Logger.ts';
+import { SubscriptionRepository } from './services/SubscriptionRepository.ts';
+import { TelegramBotController } from './services/TelegramBotController.ts';
+import { TwitchApi } from './services/TwitchApi.ts';
+
+const telegramBot = new TelegramBot(config.telegram.botToken, {
+	polling: true,
+});
+const database = new Database(config.database.url);
+const subsRepo = new SubscriptionRepository({ database });
+const twitchApi = new TwitchApi(config.twitch, { logger });
+
+new TelegramBotController({
+	telegramBot,
+	subsRepo,
+	twitchApi,
+	logger,
+});
+
+setInterval(() => {}, 1000 * 60 * 5);
