9b41235d2730ab533ec4303997086bcc2349b03c
spanwite <spanwite@ya.ru>
2025-12-07 11:58:46 +0300

    refactor(telegram-bot): integrate bottleneck package
    Replace custom limiter implementations to Bottleneck class

--- src/TelegramBot/Bot.ts
+++ src/TelegramBot/Bot.ts
@@ -1,51 +1,51 @@
+import Bottleneck from 'bottleneck';
 import { default as NodeTelegramBot } from 'node-telegram-bot-api';
-import { makeIntervalLimiter } from '../utils/function.ts';
 
 export class TelegramBot extends NodeTelegramBot {
-	private readonly globalLimiter = makeIntervalLimiter(1000, 30);
-	private readonly chatLimiter = makeIntervalLimiter(1000, 1);
+	protected readonly globalLimiter = new Bottleneck({
+		reservoir: 30,
+		reservoirRefreshAmount: 30,
+		reservoirRefreshInterval: 1000,
+		maxConcurrent: 3,
+		minTime: 33,
+	});
+	protected readonly chatLimiters = new Bottleneck.Group({
+		minTime: 1000,
+		maxConcurrent: 1,
+	});
+
+	constructor(token: string, options?: NodeTelegramBot.ConstructorOptions) {
+		super(token, options);
+		this.chatLimiters.on('created', (limiter) => {
+			limiter.chain(this.globalLimiter);
+		});
+	}
 
 	override sendMessage(
 		chatId: NodeTelegramBot.ChatId,
 		text: string,
 		options?: NodeTelegramBot.SendMessageOptions,
 	): Promise<NodeTelegramBot.Message> {
-		return this.globalLimiter.schedule(() =>
-			this.chatLimiter.schedule(
-				super.sendMessage.bind(this),
-				chatId.toString(),
-				chatId,
-				text,
-				options,
-			),
-		);
+		return this.chatLimiters
+			.key(String(chatId))
+			.schedule(() => super.sendMessage(chatId, text, options));
 	}
 
 	override editMessageText(
 		text: string,
 		options?: NodeTelegramBot.EditMessageTextOptions,
 	): Promise<NodeTelegramBot.Message | boolean> {
-		return this.globalLimiter.schedule(() =>
-			this.chatLimiter.schedule(
-				super.editMessageText.bind(this),
-				options?.chat_id?.toString() ?? 'default',
-				text,
-				options,
-			),
-		);
+		const key = options?.chat_id?.toString() ?? 'default';
+		return this.chatLimiters.key(key).schedule(() => super.editMessageText(text, options));
 	}
 
 	override editMessageReplyMarkup(
 		replyMarkup: NodeTelegramBot.InlineKeyboardMarkup,
 		options?: NodeTelegramBot.EditMessageReplyMarkupOptions,
 	): Promise<NodeTelegramBot.Message | boolean> {
-		return this.globalLimiter.schedule(() =>
-			this.chatLimiter.schedule(
-				super.editMessageReplyMarkup.bind(this),
-				options?.chat_id?.toString() ?? 'default',
-				replyMarkup,
-				options,
-			),
-		);
+		const key = options?.chat_id?.toString() ?? 'default';
+		return this.chatLimiters
+			.key(key)
+			.schedule(() => super.editMessageReplyMarkup(replyMarkup, options));
 	}
 }
